49b9fb74556273d56c2597b15ff27facda4ef0fd
houpuling <houpuling@msunsoft.com>
2022-01-31 15:32:37 +0800

    AsyncWebServerHost

--- AsyncWebServerHost/AsyncWebServerHost.ino
+++ AsyncWebServerHost/AsyncWebServerHost.ino
@@ -1,6 +1,7 @@
 #include <WiFi.h>
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
+#include <BleKeyboard.h>
 
 // Replace with your network credentials
 const char* ssid = "iQOO Z1";
@@ -12,38 +13,39 @@ const char* PARAM_INPUT_2 = "state";
 // Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
 
-const char index_html[] PROGMEM = R"rawliteral(
-<!DOCTYPE HTML><html>
-<head>
-  <title>ESP Web Server</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="icon" href="data:,">
-  <style>
-    html {font-family: Arial; display: inline-block; text-align: center;}
-    h2 {font-size: 3.0rem;}
-    p {font-size: 3.0rem;}
-    body {max-width: 600px; margin:0px auto; padding-bottom: 25px;}
-    .switch {position: relative; display: inline-block; width: 120px; height: 68px} 
-    .switch input {display: none}
-    .slider {position: absolute; top: 0; left: 0; right: 0; bottom: 0; background-color: #ccc; border-radius: 6px}
-    .slider:before {position: absolute; content: ""; height: 52px; width: 52px; left: 8px; bottom: 8px; background-color: #fff; -webkit-transition: .4s; transition: .4s; border-radius: 3px}
-    input:checked+.slider {background-color: #b30000}
-    input:checked+.slider:before {-webkit-transform: translateX(52px); -ms-transform: translateX(52px); transform: translateX(52px)}
-  </style>
-</head>
-<body>
-  <h2>ESP Web Server</h2>
-  %BUTTONPLACEHOLDER%
-<script>function toggleCheckbox(element) {
-  var xhr = new XMLHttpRequest();
-  if(element.checked){ xhr.open("GET", "/update?output="+element.id+"&state=1", true); }
-  else { xhr.open("GET", "/update?output="+element.id+"&state=0", true); }
-  xhr.send();
-}
-</script>
-</body>
-</html>
-)rawliteral";
+const char index_html[] PROGMEM = R"rawliteral(hello)rawliteral";
+//const char index_html[] PROGMEM = R"rawliteral(
+//<!DOCTYPE HTML><html>
+//<head>
+//  <title>ESP Web Server</title>
+//  <meta name="viewport" content="width=device-width, initial-scale=1">
+//  <link rel="icon" href="data:,">
+//  <style>
+//    html {font-family: Arial; display: inline-block; text-align: center;}
+//    h2 {font-size: 3.0rem;}
+//    p {font-size: 3.0rem;}
+//    body {max-width: 600px; margin:0px auto; padding-bottom: 25px;}
+//    .switch {position: relative; display: inline-block; width: 120px; height: 68px} 
+//    .switch input {display: none}
+//    .slider {position: absolute; top: 0; left: 0; right: 0; bottom: 0; background-color: #ccc; border-radius: 6px}
+//    .slider:before {position: absolute; content: ""; height: 52px; width: 52px; left: 8px; bottom: 8px; background-color: #fff; -webkit-transition: .4s; transition: .4s; border-radius: 3px}
+//    input:checked+.slider {background-color: #b30000}
+//    input:checked+.slider:before {-webkit-transform: translateX(52px); -ms-transform: translateX(52px); transform: translateX(52px)}
+//  </style>
+//</head>
+//<body>
+//  <h2>ESP Web Server</h2>
+//  %BUTTONPLACEHOLDER%
+//<script>function toggleCheckbox(element) {
+//  var xhr = new XMLHttpRequest();
+//  if(element.checked){ xhr.open("GET", "/update?output="+element.id+"&state=1", true); }
+//  else { xhr.open("GET", "/update?output="+element.id+"&state=0", true); }
+//  xhr.send();
+//}
+//</script>
+//</body>
+//</html>
+//)rawliteral";
 
 // Replaces placeholder with button section in your web page
 String processor(const String& var){
@@ -66,6 +68,9 @@ String outputState(int output){
     return "";
   }
 }
+
+
+BleKeyboard bleKeyboard;
 
 void setup(){
   // Serial port for debugging purposes
@@ -117,6 +122,11 @@ void setup(){
 
   // Start server
   server.begin();
+
+
+  bleKeyboard.begin();
+  while (!bleKeyboard.isConnected());
+  Serial.println("BLE conected");
 }
 
 // These two variables are to make sure there will not be counted repeatlly.
